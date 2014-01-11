0632bebff8dcd790052fa3da5599f7102c3de8b3
Saeed Richardson <srichardson@virtualevals.org>
2014-01-11 15:42:44 -0600

    http://book.discovermeteor.com/chapter/creating-posts Creating Posts

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,39 +1,5 @@
-<<<<<<< HEAD
-// var postsData = [
-//      {
-//          title: 'Introducing Telescope',
-//          author: 'Sacha Greif',
-//          url: 'http://sachagreif.com/introducing-telescope/'
-//      }, 
-//      {
-//          title: 'Meteor',
-//          author: 'Tom Coleman',
-//          url: 'http://meteor.com'
-//      }, 
-//      {
-//          title: 'The Meteor Book',
-//          author: 'Tom Coleman',
-//          url: 'http://themeteorbook.com'
-//      }
-// ];
-
 Template.postsList.helpers({
-    posts: function() {
-        return Posts.find();
-    }
-});
-=======
-Template.postsList.helpers({
-  postsWithRank: function() {
-    this.posts.rewind();
-    return this.posts.map(function(post, index, cursor) {
-      post._rank = index;
-      return post;
-    });
-  },
-  hasMorePosts: function(){
-    this.posts.rewind();
-    return Router.current().limit() == this.posts.fetch().length;
+  posts: function() {
+    return Posts.find();
   }
 });
->>>>>>> 97db48e8d1e0bf4a4abed9d1a0fd1d8a2b85e9ee
